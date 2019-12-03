90438bcf82ab157682f8990fd746f01a2062fa18
MwlLj <731025894@qq.com>
2019-12-03 19:20:33 +0800

    修复错误

--- src/parser/stream.rs
+++ src/parser/stream.rs
@@ -52,7 +52,7 @@ impl CParser {
             let c = *c;
             match charMode {
                 CharMode::Normal => {
-                    if c == c == b'"' || c == b'^' {
+                    if c == b'"' || c == b'^' {
                         match charMode {
                             CharMode::BackQuote => {
                             },
